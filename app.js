4215e1c37efeec1b34d5696292e1bf53fb0004db
sameer ahmed nasrullah <sameerahmednasrullah@gmail.com>
2021-04-18 09:29:33 +0500

    Work  Comlete

---
+++ app.js
@@ -0,0 +1,57 @@
+// alert("hello javaScript")
+
+
+
+
+// alert("what is your name " + " my name is umar")
+
+
+
+// alert("what is your name  \n my name is umar")
+
+
+// var data = "this is value of data 30"
+
+
+// alert(data);
+
+
+
+
+
+
+  var data  = 'umar';
+  var data1 = parseInt(prompt('enter add Number'))
+  var data2 = parseInt(prompt('enter add Number'))
+  alert(data1+data2)
+
+  if(data === data1){
+      alert(data);
+
+
+  }  
+  else if(data[2] ===data1 ){
+    alert(data[2]);
+
+  }
+  else if(data[1] ===data1 ){
+    alert(data[2]);
+
+  }
+  else{
+      alert(data+"fail");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
